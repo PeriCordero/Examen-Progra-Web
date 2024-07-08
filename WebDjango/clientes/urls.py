cf64f9d2169cee906f299f92fddea17ba3922492
PeriCordero <periclestoon@gmail.com>
2024-07-07 23:30:27 -0400

    prueba progra web portafolio

--- WebDjango/clientes/urls.py
+++ WebDjango/clientes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about_us', views.about_us, name='about_us'),
     path('portfolio', views.portfolio, name='portfolio'),
     path('portfolio_details', views.portfolio_details, name='portfolio_details'),
